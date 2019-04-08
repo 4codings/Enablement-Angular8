73ea729d43d1b24bd5893f08c69d248c91cbdbd0
Hemant-Patidar <hemant.eblue@gmail.com>
2019-04-08 07:20:42 +0530

    Update UI

--- src/app/home/useradmin/useradmin.module.ts
+++ src/app/home/useradmin/useradmin.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { UseradminComponent } from './useradmin.component';
 import { UserAdminUserComponent } from './user-admin-user/user-admin-user.component';
 import { UseradminRoutingModule } from './useradmin-routing.module';
-import { MatIconModule, MatListModule } from '@angular/material';
+import { MatIconModule, MatListModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [UseradminComponent, UserAdminUserComponent],
   imports: [
     MatIconModule,
     MatListModule,
+    MatCardModule,
     CommonModule,
     UseradminRoutingModule
   ]
