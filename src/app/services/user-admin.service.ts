fd7d5ddce1fe61d83353d58e5821a9af6516780b
4codings <proudofdeveloper@gmail.com>
2019-04-14 23:34:41 +0530

    Added token in the http request headers

--- src/app/services/user-admin.service.ts
+++ src/app/services/user-admin.service.ts
@@ -6,61 +6,45 @@ import { userGroup } from '../store/user-admin/user-group/usergroup.model';
 import { userRole } from '../store/user-admin/user-role/userrole.model';
 import { userMemberShip } from '../store/user-admin/user-membership/usermembership.model';
 import { AuthorizationData } from '../store/user-admin/user-authorization/authorization.model';
+import { userInfo } from '../store/auth/userinfo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAdminService {
-  sessionData: any[] = []
-	constructor(private http:HttpClient,
-		private authHeader: HttpHeaders
-		) {
-			this.authHeader = new HttpHeaders({ 'Authorization': `Bearer`+ this.sessionData['TOKEN'] })
-
-  if(sessionStorage.getItem('u') != undefined || sessionStorage.getItem('u') != null){
-	const data = JSON.parse(sessionStorage.getItem('u'))
-  this.sessionData = data
-  console.log(this.sessionData['SRC_CD'])
+    sessionDataToken;
+	constructor(private http:HttpClient) {
+		if(sessionStorage.getItem('u') != undefined || sessionStorage.getItem('u') != null){
+		    const data = JSON.parse(sessionStorage.getItem('u'))
+			this.sessionDataToken = data.TOKEN;
+		}
 	}
-}
 
 	
     getUsers(): Observable<User[]> {
-		return this.http.get<User[]>('https://enablement.us/Enablement/rest/v1/SPJSON?V_CD_TYP=USER&V_SRC_CD='+this.sessionData['SRC_CD']+'&REST_Service=Masters&Verb=GET',{ headers: this.authHeader });
+		let header = new HttpHeaders().set('Authorization', `Bearer ${this.sessionDataToken}`);
+		return this.http.get<User[]>('https://enablement.us/Enablement/rest/v1/securedJSON?V_CD_TYP=USER&amp;V_SRC_CD=cbp%207&amp;REST_Service=Masters&amp;Verb=GET', {headers: header});
 	}
 
 	getUserGroups(): Observable<userGroup[]> {
-		return this.http.get<userGroup[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_SRC_CD=uttra.24&V_CD_TYP=USR_GRP&REST_Service=Masters&Verb=GET');
+		let header = new HttpHeaders().set('Authorization', `Bearer ${this.sessionDataToken}`);
+		return this.http.get<userGroup[]>('https://enablement.us/Enablement/rest/v1/securedJSON?V_CD_TYP=USR_GRP&V_SRC_CD=cbp%207&REST_Service=Masters&Verb=GET', {headers: header});
 	}
 
 	getUserRoles(): Observable<userRole[]> {
-		return this.http.get<userRole[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_CD_TYP=ROLE&V_SRC_CD=AB&REST_Service=Masters&Verb=GET');
+		let header = new HttpHeaders().set('Authorization', `Bearer ${this.sessionDataToken}`);
+		return this.http.get<userRole[]>('https://enablement.us/Enablement/rest/v1/securedJSON?V_CD_TYP=ROLE&V_SRC_CD=cbp%207&REST_Service=Masters&Verb=GET', {headers: header});
 	}
 
 	//membership-> user/group
 	getUserMemberShip(): Observable<userMemberShip[]> {
-		return this.http.get<userRole[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_CD_TYP=ROLE&V_SRC_CD=AB&REST_Service=Masters&Verb=GET');
+		let header = new HttpHeaders().set('Authorization', `Bearer ${this.sessionDataToken}`);
+		return this.http.get<userRole[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_CD_TYP=ROLE&V_SRC_CD=AB&REST_Service=Masters&Verb=GET', {headers: header});
 		// return this.http.get<userMemberShip[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_SRC_CD=uttra.24&V_CD_TYP=USR_GRP&REST_Service=Masters&Verb=GET');
 	}
 
 	getAuthorizationData(): Observable<AuthorizationData[]>{
-<<<<<<< HEAD
-    return this.http.get<AuthorizationData[]>('https://enablement.us/Enablement/rest/E_DB/SPJSON?V_CD_TYP=AUTH&V_SRC_CD=uttra.24&REST_Service=Masters&Verb=GET');
-	}
-	
-
-	getHeader(){
-		if(sessionStorage.getItem('u') != undefined || sessionStorage.getItem('u') != null){
-			var data = JSON.parse(sessionStorage.getItem('u'))
-			console.log(data)
-		}
-		let getHeaders: HttpHeaders = new HttpHeaders({
-			'Content-Type': 'application/json',
-			'Authorization': 'Bearer' +data.token
-		});
-		return getHeaders;
-=======
-    return this.http.get<AuthorizationData[]>('https://enablement.us/Enablement/rest/v1/securedJSON?V_CD_TYP=AUTH&V_SRC_CD=cbp%207&REST_Service=Masters&Verb=GET');
->>>>>>> 3b81aad9b11bcf0f9ea948e254ce059f5a007363
+		let header = new HttpHeaders().set('Authorization', `Bearer ${this.sessionDataToken}`);
+    	return this.http.get<AuthorizationData[]>('https://enablement.us/Enablement/rest/v1/securedJSON?V_CD_TYP=AUTH&V_SRC_CD=cbp%207&REST_Service=Masters&Verb=GET', {headers: header});
 	}
 }
