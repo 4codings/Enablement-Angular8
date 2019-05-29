4529edba600f2b976bba0cdca2ba88709b44803d
Javkhlantugs Nyamdorj <javkhaanj7@gmail.com>
2019-05-30 02:27:15 +0800

    Updated prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   domainName: 'https://enablement.us/Enablement/',
   apiURL: 'https://enablement.us/',
-  idle: 10,
-  timeout: 20
+  idle: 780,
+  timeout: 120
 };
