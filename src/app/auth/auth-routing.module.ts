a2040e7bcc64020358b341c149fc883c8f0f2b0a
hiren.parekh <parekhhiren@outlook.com>
2019-06-08 12:36:35 +0530

    Lazy load issue fixed

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,24 +1,24 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from './auth.component';
-import { AnonymousOnlyGuard } from '../core/guards/anonymous-only.guard';
+import {AnonymousOnlyGuard} from '../core/guards/anonymous-only.guard';
 // import {AnonymousOnlyGuard} from "../core/guards/anonymous-only.guard";
 
 
 const routes: Routes = [
-    {
-        path: '', loadChildren: './login/login.module#LoginModule',
-        canActivate: [AnonymousOnlyGuard]
-    },
+  {
+    path: '',
+    loadChildren: './login/login.module#LoginModule',
+    canActivate: [AnonymousOnlyGuard]
+  },
 ];
 
-
 @NgModule({
-    imports:      [
-        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
-    ],
-    exports:      [RouterModule],
-    declarations: []
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AuthRoutingModule {
 }
