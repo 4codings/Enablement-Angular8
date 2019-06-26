cff61d0dce93f357ee0005237028a8e06fdf0bfc
Javkhlantugs Nyamdorj <javkhaanj7@gmail.com>
2019-06-26 21:26:18 +0800

    Fix and upgrade bpmn version

--- src/app/home/enduser/process-design/bpmn-js.ts
+++ src/app/home/enduser/process-design/bpmn-js.ts
@@ -1,4 +1,4 @@
-import _Modeler from 'bpmn-js/lib/Modeler.js';
+import * as _Modeler from 'bpmn-js/dist/bpmn-modeler.production.min.js';
 import _PropertiesPanelModule from 'bpmn-js-properties-panel';
 import _BpmnPropertiesProvider from 'bpmn-js-properties-panel/lib/provider/bpmn';
 import _EntryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
