002546a49d6b87e29d3d704db7909c658dcb33f2
4codings <proudofdeveloper@gmail.com>
2019-08-03 00:06:53 +0530

    deleted all spec files

--- src/app/home/system-admin/overview/pipes/filter-exetype.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterExetypePipe } from './filter-exetype.pipe';
-
-describe('FilterExetypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterExetypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/home/system-admin/overview/pipes/filter-machines.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterMachinesPipe } from './filter-machines.pipe';
-
-describe('FilterMachinesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterMachinesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
