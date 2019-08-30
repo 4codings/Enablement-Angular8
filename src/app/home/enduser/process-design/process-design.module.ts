29821735d1c3a950d390edea7bdc256d176d3731
4codings <proudofdeveloper@gmail.com>
2019-08-30 22:24:50 +0530

    design changes added collapse expansion

--- src/app/home/enduser/process-design/process-design.module.ts
+++ src/app/home/enduser/process-design/process-design.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { ProcessDesignRoutingModule } from './process-design-routing.module';
-import { ProcessDesignComponent } from './process-design.component';
+import { ProcessDesignComponent, IsOutsideDirective } from './process-design.component';
 import { HeaderModule } from '../../header/header.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModalModule } from 'ngx-bootstrap/modal';
@@ -12,7 +12,7 @@ import { ResolveComponent } from './resolve/resolve.component';
 import { ApproveComponent } from './approve/approve.component';
 
 @NgModule({
-  declarations: [ProcessDesignComponent, SchedularComponent, MonitorComponent, ResolveComponent, ApproveComponent],
+  declarations: [ProcessDesignComponent, SchedularComponent, MonitorComponent, ResolveComponent, ApproveComponent, IsOutsideDirective],
   imports: [
     SharedModule,
     ProcessDesignRoutingModule,
