2349e850eaec4b302b6e544fae0f15832786d1f3
4codings <proudofdeveloper@gmail.com>
2019-09-03 23:57:26 +0530

    Update add exe & conn component

--- src/app/home/system-admin/overview/dialogs/edit-connection-dialog/edit-connection-dialog.component.ts
+++ src/app/home/system-admin/overview/dialogs/edit-connection-dialog/edit-connection-dialog.component.ts
@@ -79,7 +79,7 @@ export class EditConnectionDialogComponent implements OnInit {
       "V_PARAM_N":V_PARAM_N,
       "V_PARAM_V":V_PARAM_V,
       "V_PLATFORM_CD":this.PLF_CD,
-      "V_PLATFORM_DCS":this.PLF_DSC.toString(),
+      "V_PLATFORM_DSC":this.PLF_DSC.toString(),
       "REST_Service":["CXN"],
       "Verb":["PUT"]
     }
