ad58092268e6cecb9425d1942a825a67b6676568
4codings <proudofdeveloper@gmail.com>
2019-11-04 00:39:20 +0530

    Fixed rebuilding problem for .cc & .us

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  domainName: 'enablement.us/Enablement',
-  apiURL: 'enablement.us',
+  domainName: 'enablement.cc/Enablement',
+  apiURL: 'enablement.cc',
   idle: 780,
   timeout: 120
 };
