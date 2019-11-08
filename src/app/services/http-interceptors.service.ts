17edd721dcb29709013bfbaa2d34e5d03efba83e
4codings <proudofdeveloper@gmail.com>
2019-11-08 20:28:58 +0530

    Session timeout issue resolved

--- src/app/services/http-interceptors.service.ts
+++ src/app/services/http-interceptors.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserService } from '../core/user.service';
 import { UserIdleService } from 'angular-user-idle';
+import { tap } from 'rxjs/operators';
+import { ApiService } from '../service/api/api.service';
+import { Router } from '@angular/router';
+import { OptionalValuesService } from './optional-values.service';
+import { RollserviceService } from './rollservice.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +16,57 @@ export class HttpInterceptorsService implements HttpInterceptor {
 
   constructor(
     private userService: UserService,
-    private userIdle: UserIdleService
+    private userIdle: UserIdleService,
+    private router: Router,
+    private optionalService: OptionalValuesService,
+    private rollserviceService: RollserviceService,
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.userIdle.resetTimer();
-    var sessiondata =  this.userService.getDetailFromStorage();
-    
-    if(sessiondata){
+    var sessiondata = this.userService.getDetailFromStorage();
+
+    if (sessiondata) {
       const cloned = request.clone({
         headers: request.headers.set("Authorization", `Bearer ${sessiondata.TOKEN}`)
       });
-      return next.handle(cloned);
+      return next.handle(cloned).pipe(tap((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          // do stuff with response if you want
+        }
+      }, (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this.sessionLogout();
+          }
+        }
+      }));
     } else {
-      return next.handle(request);
+      return next.handle(request)
+        .pipe(tap((event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+            // do stuff with response if you want
+          }
+        }, (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              this.sessionLogout();
+            }
+          }
+        }));
     }
+  }
+  sessionLogout() {
+    this.optionalService.applicationOptionalValue.next(null);
+    this.optionalService.processOptionalValue.next(null);
+    this.optionalService.serviceOptionalValue.next(null);
+    this.optionalService.applicationProcessValue.next(null);
+    this.optionalService.applicationArray = [];
+    this.optionalService.serviceArray = [];
+    this.optionalService.processArray = [];
+    this.optionalService.applicationProcessArray = [];
+    this.userService.clear();
+    this.rollserviceService.clear();
+    this.router.navigateByUrl('/login', { skipLocationChange: true });
   }
 }
